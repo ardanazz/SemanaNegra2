77568a1ecd7b9c47680f26ab67af59b3ea30faa1
ardanazz <adreciko@gmail.com>
2017-09-28 12:18:14 +0200

    Comit pavooo

---
+++ app/src/main/java/com/example/usuario/myapplication/InfoActivity.java
@@ -0,0 +1,18 @@
+package com.example.usuario.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class InfoActivity extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+    }
+}
