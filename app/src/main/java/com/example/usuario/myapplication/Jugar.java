e2026442d816fba57c86a4cb167a9f4246c56464
Adrián Reina Goñi <aereoforce@gmail.com>
2017-10-05 12:08:41 +0200

    Permisos

--- app/src/main/java/com/example/usuario/myapplication/Jugar.java
+++ app/src/main/java/com/example/usuario/myapplication/Jugar.java
@@ -1,10 +1,12 @@
 package com.example.usuario.myapplication;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,6 +14,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.security.Permission;
+import java.security.PermissionCollection;
+import java.security.Permissions;
 
 public class Jugar extends FragmentActivity implements OnMapReadyCallback {
 
@@ -25,6 +31,10 @@ public class Jugar extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 25);
+        }
     }
 
 
@@ -46,17 +56,24 @@ public class Jugar extends FragmentActivity implements OnMapReadyCallback {
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+
+
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 25) {
+            if (permissions.length == 1 &&
+                    permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION) &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permiso concedido", Toast.LENGTH_LONG).show();
+                mMap.setMyLocationEnabled(true);
+            } else {
+                Toast.makeText(this, "Permiso denegado", Toast.LENGTH_LONG).show();
+                this.finish();
+            }
         }
-        mMap.setMyLocationEnabled(true);
 
     }
 }
--- app/src/main/java/com/example/usuario/myapplication/MainActivity.java
+++ app/src/main/java/com/example/usuario/myapplication/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+
+
     Button siguiente, next;
 
             protected void onCreate(Bundle savedInstanceState) {
